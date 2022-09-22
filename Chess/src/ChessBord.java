98cb264033eac03f32a130d4560975b88f4e6eb5
RobsonMaia <robsonmaia53@gmail.com>
2022-09-22 17:30:08 -0300

    add coments

--- Chess/src/ChessBord.java
+++ Chess/src/ChessBord.java
@@ -1,10 +1,13 @@
 
 public class ChessBord {
 
-	static private ChessBord instance = null;
+	static private ChessBord instance = null; //instancia unica
+	/*instance = null PRA NÃO FICAR CRIANDO OBJETOS SEMPRE QUE EU RODAR O PROGRAMA
+	 *if (instance == null) SO VAI CRIAR O OBJETO SE ELE AINDA NÃO FOI CRIADO
+	 * */
 	
-	private ChessBord( ) {}
-	static ChessBord getInstance() {
+	private ChessBord( ) {} //privado pra nenhuma outra classe criar outra instancia
+	static ChessBord getInstance() { //modo de chamar a instancia
 		if (instance == null) {
 			instance = new ChessBord();
 			return instance;
@@ -16,7 +19,7 @@ public class ChessBord {
 	public static void main (String args[])
 	{
 	
-		ChessBord tabuleiro = ChessBord.getInstance();
+		ChessBord tabuleiro = ChessBord.getInstance(); //objeto 
 		ChessBord tabuleiro1 = ChessBord.getInstance();
 		System.out.println(tabuleiro);
 		System.out.println(tabuleiro1);
